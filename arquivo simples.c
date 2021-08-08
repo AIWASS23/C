67733d1c69234a9bf8aea9f7ef27401e699af8cc
Marcelo De Araújo <84019436+AIWASS23@users.noreply.github.com>
2021-08-08 06:26:39 -0300

    Add files via upload

---
+++ arquivo simples.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+    FILE *X;
+    char P;
+
+
+X = fopen("arq.txt", "w");
+if (X == NULL) {
+ fprintf(stderr,"\nfopen() failed in file %s at line # %d", __FILE__,__LINE__);
+ exit(EXIT_FAILURE);
+}
+printf("\nEntre com um caracter qualquer ou 0 para terminar:");
+P = getchar();
+fflush(stdin); 
+while (P != '0')
+{
+   fputc(P,X);
+   printf("\nEntre com um caracter qualquer ou 0 para terminar:");
+   P = getchar();
+   fflush(stdin); 
+}
+fclose(X);
+X = fopen("arq.txt", "r");
+if (X == NULL){
+	
+   fprintf(stderr,"\nfopen() failed in file %s at line # %d", __FILE__,__LINE__);
+   exit(EXIT_FAILURE);
+}
+printf("\n\nVou ler e mostrar o texto gravado....\n\n");
+P = fgetc(X);
+while (P != EOF){
+	
+   printf("%c",P);
+   P =fgetc(X);
+}
+
+fclose(X);
+
+}
---
+++ maiusculas e minisculas.c
@@ -0,0 +1,61 @@
+﻿#include <stdio.h>
+#include<ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	
+   FILE *arq;
+   int i,j;
+   char texto[50];
+   char maiusc[50];
+   char nome_arq[20];
+   int num;
+   char c;
+   
+    printf("\n\nDigite o nome do arquivo a ser criado: ");
+    scanf("%s", &nome_arq);
+    scanf("%c",&c);
+    arq = fopen(nome_arq, "w");
+   
+    if (arq == NULL) {
+         fprintf(stderr,"fopen() failed in file %s at line # %d", __FILE__,__LINE__);
+         exit(EXIT_FAILURE);
+    }
+
+    for (i=0; i < 2; i++){
+      printf("\n\nDigite uma frase com 49 caracteres máximo: ");
+      gets(texto);
+      fflush(stdin);
+      j=0;
+      
+        while ( texto[j] != '\0' && j < 49){
+        maiusc[j] = toupper(texto[j]);
+        j++;
+      }
+        maiusc[j]='\0';
+
+        fwrite(&num, sizeof(int), 1, arq);
+
+        fwrite(maiusc, sizeof(char), num, arq);
+    }
+
+   fclose(arq);
+   arq = fopen(nome_arq, "r");
+    if (arq == NULL) {
+         fprintf(stderr,"fopen() failed in file %s at line # %d", __FILE__,__LINE__);
+         exit(EXIT_FAILURE);
+    }
+
+    fread(&num, sizeof(int),1,arq);
+    fread(texto, sizeof(char),num, arq);
+    
+   while (!feof (arq) ){
+      printf("\n\nTexto Lido: %s", texto);
+      memset(texto,'\0',50);
+      fread(&num, sizeof(int),1,arq);
+      fread(texto, sizeof(char),num, arq);
+
+    }
+    fclose(arq);
+}
